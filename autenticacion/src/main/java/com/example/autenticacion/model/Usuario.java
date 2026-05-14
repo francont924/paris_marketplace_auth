b81eec125bc36faf7c7f70e26a82567e0bcbec61
francont924 <fran.contreraass@duocuc.cl>
2026-05-13 22:57:35 -0400

    avance proyecto(model, usuario)

---
+++ autenticacion/src/main/java/com/example/autenticacion/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.example.autenticacion.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "Usuarios")
+
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String nombre;
+}
